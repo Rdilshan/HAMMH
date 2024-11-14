be6d0cee624552441e74dbc84bd095d106ac2684
Bimsara <ravindu.bimsara97@gmail.com>
2024-11-14 22:53:21 +0530

    responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,7 +7,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
     <div className="min-h-screen bg-[#F8F3FF]">
       <Sidebar />
       <Navbar />
-      <main className="sm:ml-2 lg:ml-64 pt-16 p-8">
+      <main className="sm:ml-18 md:ml-24 lg:ml-64 pt-16 p-8">
         {children}
       </main>
     </div>
