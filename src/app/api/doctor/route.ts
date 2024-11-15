018b5592afc76403f536504e2de3ef0955878049
Randika Dilshan <rdilshan077788@gmail.com>
2024-11-15 10:23:56 +0530

    create doctor profile

---
+++ src/app/api/doctor/route.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { genSaltSync, hashSync } from "bcrypt-ts";
+
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const doctors = await prisma.user.findMany();
+    return NextResponse.json(doctors); // Return response here
+  } catch (error) {
+    console.error("Error fetching doctors:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch doctors" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+
+    const salt = genSaltSync(10);
+    const hash = hashSync(data.password, salt);
+    // const hash = bcrypt.hashSync(, process.env.PWD_SALT!);
+    const newDoctor = await prisma.user.create({
+      data: {
+        name: data.name,
+        email: data.email,
+        password: hash,
+        role: "doctor",
+        telephone:data.telephone,
+        Specialization:data.Specialization,
+        gender:data.gender
+      },
+    });
+
+    return NextResponse.json({message:"success create doctor"},{status:200}); 
+    
+  } catch (error) {
+    console.error("Error fetching doctors:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch doctors" },
+      { status: 500 }
+    );
+  }
+}
