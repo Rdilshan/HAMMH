a75087d69e5212e0701c098ab88d6ae646780fc4
Randika Dilshan <rdilshan077788@gmail.com>
2024-11-15 13:36:36 +0530

    decrypt the token exclude the api/Auth route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,45 @@
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import * as jose from "jose";
 
-export function middleware(request: NextRequest) {
-    console.log("middleware.....");
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function middleware(request: NextRequest) {
+
+  //exclude the /api/Auth route
+  const routepath = request.nextUrl.pathname;
+  if(routepath == "/api/Auth"){
     return NextResponse.next();
-    // return NextResponse.redirect(new URL('/home', request.url));
+  }
+  const authHeader = request.headers.get("authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return NextResponse.json(
+      { message: "Authorization header missing or invalid" },
+      { status: 401 }
+    );
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decodedToken = await jose.jwtVerify(token, secret);
+
+    const payloadData = decodedToken.payload.data as { role: string };
+    const role: string = payloadData.role;
+    console.log(role);
+
+    return NextResponse.next();
+
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Invalid or expired token" },
+      { status: 403 }
+    );
+  }
+
+  // return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/:path*',
+  matcher: "/api/:path*",
 };
+
