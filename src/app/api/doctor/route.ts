08f50dc89bf8baedd799f03b24b5def4c990b40e
Randika Dilshan <rdilshan077788@gmail.com>
2024-11-29 06:08:47 +0530

    doctor register & send normal email

--- src/app/api/doctor/route.ts
+++ src/app/api/doctor/route.ts
@@ -1,19 +1,17 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { genSaltSync, hashSync } from "bcrypt-ts";
-
+import EmailService from "@/app/api/email/email";
 
 const prisma = new PrismaClient();
 
 export async function GET() {
   try {
-    const doctors = await prisma.user.findMany(
-      {
-        where: {
-          role: "doctor",
-        },
-      }
-    );
+    const doctors = await prisma.user.findMany({
+      where: {
+        role: "doctor",
+      },
+    });
     return NextResponse.json(doctors); // Return response here
   } catch (error) {
     console.error("Error fetching doctors:", error);
@@ -26,27 +24,71 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
+
     const data = await request.json();
 
-     await prisma.user.create({
+
+    if (!data || Object.keys(data).length === 0) {
+      return NextResponse.json(
+        { msg: "Invalid or missing request body" },
+        { status: 400 }
+      );
+    }
+
+    const exitingdoctor = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+    if (exitingdoctor) {
+      return NextResponse.json(
+        { msg: "Email already exists" },
+        { status: 400 }
+      );
+    }
+
+    // Process the data (creating a doctor)
+    const newDoctor = await prisma.user.create({
       data: {
         name: data.name,
         email: data.email,
         role: "doctor",
-        telephone:data.telephone,
-        Specialization:data.Specialization,
-        gender:data.gender,
-        active_status:'Deactive'
+        telephone: data.telephone,
+        Specialization: data.Specialization,
+        gender: data.gender,
+        active_status: "Deactive",
       },
     });
 
-    return NextResponse.json({message:"success create doctor"},{status:200}); 
-    
-  } catch (error) {
-    console.error("Error fetching doctors:", error);
+    if (!newDoctor) {
+      return NextResponse.json(
+        { message: "Failed to create doctor" },
+        { status: 500 }
+      );
+    }
+
+    // Send email
+    const emailService = new EmailService();
+    try {
+      await emailService.sendEmail(
+        data.email,
+        "Welcome to our platform",
+        "Thank you for joining our platform!"
+      );
+    } catch (emailError) {
+      console.error("Error sending email:", emailError);
+    }
+
     return NextResponse.json(
-      { error: "Failed to fetch doctors" },
+      { message: "Doctor created successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error processing request:", error);
+    return NextResponse.json(
+      { msg: "Internal Server Error" },
       { status: 500 }
     );
   }
 }
+
--- src/app/dashboard/doctors/register/page.tsx
+++ src/app/dashboard/doctors/register/page.tsx
@@ -1,7 +1,15 @@
 'use client'
 
 import React, { useState } from 'react';
-import Image from 'next/image';
+import toast, { Toaster } from 'react-hot-toast';
+
+
+interface ApiResponse {
+  message?: string;
+  data?: any;
+  error?: string;
+}
+
 
 const DoctorRegister = () => {
   const [fullName, setFullName] = useState('');
@@ -12,7 +20,7 @@ const DoctorRegister = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     const baseUrl = `/api/doctor`;
     const response = await fetch(baseUrl, {
       method: "POST",
@@ -20,19 +28,20 @@ const DoctorRegister = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name:fullName,
-        email:email,
-        telephone:contactNumber,
-        Specialization:specialization,
-        gender:gender
+        name: fullName,
+        email: email,
+        telephone: contactNumber,
+        Specialization: specialization,
+        gender: gender
       })
     });
 
-
     if (response.status === 200) {
-      console.log("success create doctor")
+      toast.success('Patient registered successfully!');
+
     } else {
-      console.log("show the error msg..")
+      toast.error('Email already exists or Not filling the required fields!');
+
     }
 
   };
@@ -40,89 +49,89 @@ const DoctorRegister = () => {
 
   return (
     <div className="px-6 py-4 text-black">
-     
+      <Toaster position="top-center" reverseOrder={false} />
       <form onSubmit={handleSubmit}>
-      
-        <div className=" bg-white p-5 rounded-md">
-        <h2 className="text-xl font-semibold text-gray-900 mb-6">DOCTOR REGISTER</h2>
-           <div className='grid md:grid-cols-2 gap-4'>
 
-        
-          <div>
-            <label htmlFor="fullName" className="block mb-2">
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="fullName"
-              placeholder="Enter full name of doctor"
-              className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="contactNumber" className="block mb-2">
-              Contact Number
-            </label>
-            <input
-              type="text"
-              id="contactNumber"
-              placeholder="Enter Contact Number"
-              className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              value={contactNumber}
-              onChange={(e) => setContactNumber(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="block mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Enter the email"
-              className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="specialization" className="block mb-2">
-              Specialization
-            </label>
-            <input
-              type="text"
-              id="specialization"
-              placeholder="Enter specialization"
-              className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              value={specialization}
-              onChange={(e) => setSpecialization(e.target.value)}
-            />
-          </div>
-          <div>
-            <label htmlFor="gender" className="block mb-2">
-              Gender
-            </label>
-            <select
-              id="gender"
-              value={gender}
-              onChange={(e) => setGender(e.target.value)}
-              className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+        <div className=" bg-white p-5 rounded-md">
+          <h2 className="text-xl font-semibold text-gray-900 mb-6">DOCTOR REGISTER</h2>
+          <div className='grid md:grid-cols-2 gap-4'>
+
+
+            <div>
+              <label htmlFor="fullName" className="block mb-2">
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                placeholder="Enter full name of doctor"
+                className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                value={fullName}
+                onChange={(e) => setFullName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="contactNumber" className="block mb-2">
+                Contact Number
+              </label>
+              <input
+                type="text"
+                id="contactNumber"
+                placeholder="Enter Contact Number"
+                className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                value={contactNumber}
+                onChange={(e) => setContactNumber(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter the email"
+                className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="specialization" className="block mb-2">
+                Specialization
+              </label>
+              <input
+                type="text"
+                id="specialization"
+                placeholder="Enter specialization"
+                className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+                value={specialization}
+                onChange={(e) => setSpecialization(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="gender" className="block mb-2">
+                Gender
+              </label>
+              <select
+                id="gender"
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+                className="text-[13px] text-sm w-full px-6 py-4 bg-purple-50 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-purple-500"
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <div></div>
+            <button
+              type="submit"
+              className="bg-purple-600 text-white rounded-md py-2 px-4 mt-4"
             >
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
-            </select>
+              Register Doctor
+            </button>
           </div>
-          <div></div>
-          <button
-            type="submit"
-            className="bg-purple-600 text-white rounded-md py-2 px-4 mt-4"
-          >
-            Register Doctor
-          </button>
-        </div>
         </div>
       </form>
     </div>
