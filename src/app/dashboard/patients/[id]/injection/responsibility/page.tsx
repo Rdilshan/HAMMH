26fd1dd8ea87c45edf63403774e2ea4c213ba1df
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-12-05 20:30:46 +0530

    injection page

--- src/app/dashboard/patients/[id]/injection/responsibility/page.tsx
+++ src/app/dashboard/patients/[id]/injection/responsibility/page.tsx
@@ -1,34 +1,73 @@
 'use client'
 import React, { useState } from "react";
+import { useRouter, useParams } from 'next/navigation';
+import toast, { Toaster } from 'react-hot-toast';
 
 const ResponsibilityForm = () => {
+  const { id } = useParams();
+  const [doctorName , setdoctorName ] = useState('');
+  const [nurseName, setnurseName] = useState('');
+  const [socialWorkers, setsocialWorkers] = useState('');
+  const [drugType, setdrugType] = useState('');
+  const [Date ,setDate ] = useState('');
+  const [NextDate ,setNextDate ] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
  
-  const [formData, setFormData] = useState({
-    doctorName: "",
-    nurseName: "",
-    socialWorker: "",
-    drugType: "",
-    date: "",
-    nextDate: "",
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
  
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Form Data Submitted:", formData);
-   
-  };
+        setIsLoading(true);
+        try {
+          const baseUrl = `/api/patient/${id}/Injection`;
+            const response = await fetch(baseUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    doctorName:doctorName,
+                    nurseName:nurseName,
+                    socialWorkers:socialWorkers,
+                    drugType:drugType,
+                    Date:Date,
+                    NextDate:NextDate,
+                    patient_id: Number(`${id}`),
+                }),
+            });
+
+           
+            let data = null;
+            if (response.headers.get('Content-Type')?.includes('application/json')) {
+                data = await response.json();
+            }
+
+            if (response.ok) {
+                toast.success('Admit registered successfully!');
+                setTimeout(() => {
+                    router.push('/dashboard/patients');
+                }, 500);
+            } else {
+                const errorMsg = data?.error;
+                toast.error(errorMsg);
+                console.error(errorMsg);
+            }
+        } catch (error) {
+            toast.error('An error occurred while saving!');
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+
+
 
   return (
+    
     <form onSubmit={handleSubmit} className="p-6 bg-white shadow-md rounded-lg">
+        <Toaster position="top-center" reverseOrder={false} />
       <h2 className="text-lg font-semibold text-gray-800 mb-6">Responsibility</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -44,8 +83,8 @@ const ResponsibilityForm = () => {
             type="text"
             id="doctorName"
             name="doctorName"
-            value={formData.doctorName}
-            onChange={handleChange}
+            value={doctorName}
+            onChange={(e) => setdoctorName(e.target.value)}
             placeholder="Enter full name of doctor"
             className="w-full px-3 py-2 rounded-lg bg-[#F8F3FF] text-black outline-none "
           />
@@ -63,8 +102,8 @@ const ResponsibilityForm = () => {
             type="text"
             id="nurseName"
             name="nurseName"
-            value={formData.nurseName}
-            onChange={handleChange}
+            value={nurseName}
+            onChange={(e) => setnurseName(e.target.value)}
             placeholder="Enter full name of nurse"
             className="w-full px-3 py-2 rounded-lg bg-[#F8F3FF] text-black outline-none"
           />
@@ -80,10 +119,10 @@ const ResponsibilityForm = () => {
           </label>
           <input
             type="text"
-            id="socialWorker"
-            name="socialWorker"
-            value={formData.socialWorker}
-            onChange={handleChange}
+            id="socialWorkers"
+            name="socialWorkers"
+            value={socialWorkers}
+            onChange={(e) => setsocialWorkers(e.target.value)}
             placeholder="Enter full name of worker"
             className="w-full px-3 py-2 rounded-lg bg-[#F8F3FF] text-black outline-none"
           />
@@ -100,8 +139,8 @@ const ResponsibilityForm = () => {
           <select
             id="drugType"
             name="drugType"
-            value={formData.drugType}
-            onChange={handleChange}
+            value={drugType}
+            onChange={(e) => setdrugType(e.target.value)}
             className="w-full px-3 py-2 rounded-lg bg-[#F8F3FF] text-black outline-none"
           >
             <option value="">Select drug type</option>
@@ -119,11 +158,11 @@ const ResponsibilityForm = () => {
             Date
           </label>
           <input
-            type="date"
-            id="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
+            type="text"
+            id="Date"
+            name="Date"
+            value={Date}
+            onChange={(e) => setDate(e.target.value)}
             className="w-full px-3 py-2 rounded-lg bg-purple-50/50 border bg-[#F8F3FF] text-black outline-none"
           />
         </div>
@@ -137,11 +176,11 @@ const ResponsibilityForm = () => {
             Next Date
           </label>
           <input
-            type="date"
-            id="nextDate"
-            name="nextDate"
-            value={formData.nextDate}
-            onChange={handleChange}
+            type="text"
+            id="NextDate"
+            name="NextDate"
+            value={NextDate}
+            onChange={(e) => setNextDate(e.target.value)}
             className="w-full px-3 py-2 rounded-lg bg-[#F8F3FF] text-black outline-none"
           />
         </div>
