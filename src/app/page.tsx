68f01c0ebabac3e8d58f44466f7473d6bc36f3f3
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-12-10 22:00:22 +0530

    login page chage loding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import type { User } from '@/types/types'
 import toast, { Toaster } from 'react-hot-toast';
+import { FaSpinner } from 'react-icons/fa';
 
 export default function LoginPage() {
   const router = useRouter()
   const [user, setUser] = useState<User>({ email: '', password: '' })
+  const [isloading,setisloading] = useState(false)
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault()
+    setisloading(true)
 
     const baseUrl = `/api/Auth`;
     const response = await fetch(baseUrl, {
@@ -35,6 +38,7 @@ export default function LoginPage() {
       console.log("show the error msg..");
       toast.error(data.message || 'An error occurred');
     }
+    setisloading(false);
 
     // if (user.email && user.password) {
     //   router.push('/dashboard')
@@ -46,8 +50,8 @@ export default function LoginPage() {
     <Toaster position="top-center" reverseOrder={false} />
       <div className="max-w-md w-full space-y-8 bg-white p-8 rounded-lg shadow-md">
         <div>
-          <h1 className="text-left text-3xl font-extrabold text-gray-900">
-            Login to Your account
+          <h1 className="text-center text-3xl font-bold text-gray-900">
+            Admin Login
           </h1>
 
         </div>
@@ -83,7 +87,14 @@ export default function LoginPage() {
             type="submit"
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Login
+           {isloading ? (
+            <>
+              <FaSpinner className="animate-spin " />
+              
+            </>
+          ) : (
+            "Login"
+          )}
           </button>
         </form>
       </div>
